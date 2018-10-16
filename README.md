54754ea544df479a6ec1c676fb455863bef1b5b6
ayush-vibrant <ayushvibrant@gmail.com>
2018-10-16 18:05:39 +0530

    added config.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Serioholic-TV-Series-Reminder-Innovacer
 This is a project for HackerCamp'19 by Innovaccer: SDE-Intern (Platform) 
+
---
+++ config.py
@@ -0,0 +1,3 @@
+# Update your account credentials to act as a sender
+FROM_EMAIL = 'ayush.smtp@gmail.com' #YOUR_GMAIL_ID
+PASSWORD = '!qwerty123' # YOUR_PASSWORD
